309c561b876ab8524014f9dc239821bc17efa7fc
soohian <shonendumm@gmail.com>
2024-04-10 11:57:41 +0800

    Add one more example

--- requestpooling.py
+++ requestpooling.py
@@ -1,4 +1,21 @@
+# Example 1: 2021
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
+retry_strategy = Retry(
+    total=3,
+    backoff_factor=1,
+    status_forcelist=[429, 500, 502, 503, 504],
+    method_whitelist=["HEAD", "GET", "OPTIONS"]
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+http = requests.Session()
+http.mount("https://", adapter)
+http.mount("http://", adapter)
+
+response = http.get("https://en.wikipedia.org/w/api.php")
+
+# Example 2: 2016
 
 import requests
 
